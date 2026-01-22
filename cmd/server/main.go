488137c20074e5be18aa77fd2e9672102d8c0a2c
Aryan Padmakar Deshmukh <aryan.deshmukh@joshsoftware.com>
2026-01-22 15:18:45 +0530

    Fix CORS order

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"log"
+	"time"
+
 	"rule-based-approval-engine/internal/config"
 	"rule-based-approval-engine/internal/database"
 	"rule-based-approval-engine/internal/jobs"
 	"rule-based-approval-engine/internal/routes"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -18,30 +19,25 @@ func main() {
 	database.Connect(cfg)
 
 	router := gin.Default()
-	routes.Register(router)
-	// ✅ CORS CONFIGURATION
+
+	//CORS MUST COME BEFORE ROUTES
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Authorization", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
+		AllowHeaders:     []string{"Content-Type"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
-	// ⏱️ CRON SETUP
+
+	//Register routes AFTER middleware
+	routes.Register(router)
+
+	//CRON SETUP
 	loc, _ := time.LoadLocation("Asia/Kolkata")
 	c := cron.New(cron.WithLocation(loc))
-
-	// Run daily at 12:00 AM
 	c.AddFunc("0 0 * * *", jobs.RunAutoRejectJob)
-
 	c.Start()
 
-	log.Println("🕛 Auto-reject cron scheduled for 12:00 AM daily")
-
-	// log.Println("🚀 Server started on port", cfg.AppPort)
+	log.Println("🚀 Server started on port", cfg.AppPort)
 	router.Run(":" + cfg.AppPort)
-	// hash, _ := bcrypt.GenerateFromPassword([]byte("Lee@123"), bcrypt.DefaultCost)
-	// fmt.Println(string(hash))
-	// log.Printf(string(hash))
 }
